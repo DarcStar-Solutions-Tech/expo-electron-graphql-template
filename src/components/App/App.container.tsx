c9e618da5830ba9bbbdd64ffe081b2ca9f30a40e
Michael Harris <mharris021@hotmail.com>
2020-10-02 16:16:42 -0500

    - fixed issue with @apollo/client hooks by whitelisting the modules in electron-webpack.js, need to see if the resolutions section is still needed

--- src/components/App/App.container.tsx
+++ src/components/App/App.container.tsx
@@ -4,11 +4,14 @@ import {useReactiveVar} from "@apollo/client";
 import {userVar} from "../../graphql/schemas";
 import {User} from "../../graphql/schemas/types";
 
-export default function AppContainer() {
-    const user = useReactiveVar<User>(userVar);
+const AppContainer=()=> {
+    let user = undefined;
+    user = useReactiveVar<User>(userVar);
     return (
         <App user={user} />
     );
 }
+
+export default AppContainer;
 
 
